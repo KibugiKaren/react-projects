3d5879a211c598434163686d9c53f41845109279
kibugikaren <karenkibugi@gmail.com>
2022-05-05 14:33:57 +0300

    created
    description

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div>
+    <nav className="navbar">
+    <div className='nav-center'>
+      <div className="nav-header">
+        <a href="index.html" class="nav-logo">
+          <img src="./img/download.png" alt="movie reel" />
+        </a>
+        </div>
+        </div>
+</nav>
+
+        <section class="page">
+            <main class="hero">
+            <img src="./img/picture-8.jpg" alt="poster" />
+            </main>
+            </section>
+</div>
+  )
+}
+
+export default Navbar
